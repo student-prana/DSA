191ffb46cd090b8470854c879367e6492fc03d2d
Pranav Chavhan <pranavchavhan2470@gmail.com>
2025-10-11 09:43:20 +0530

    Time: 15 ms (95.33%) | Memory: 44.4 MB (56.65%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,78 +1,16 @@
 class Solution {
-    private int[] findNSE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
-            ans[i] = !st.isEmpty() ? st.peek() : n;
-            st.push(i);
-        }
-        return ans;
-    }
-
-    private int[] findNGE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) st.pop();
-            ans[i] = !st.isEmpty() ? st.peek() : n;
-            st.push(i);
-        }
-        return ans;
-    }
-
-    private int[] findPSEE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] > arr[i]) st.pop();
-            ans[i] = !st.isEmpty() ? st.peek() : -1;
-            st.push(i);
-        }
-        return ans;
-    }
-
-    private int[] findPGEE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] < arr[i]) st.pop();
-            ans[i] = !st.isEmpty() ? st.peek() : -1;
-            st.push(i);
-        }
-        return ans;
-    }
-
-    private long sumSubarrayMins(int[] arr) {
-        int n = arr.length;
-        int[] nse = findNSE(arr);
-        int[] psee = findPSEE(arr);
+   public long subArrayRanges(int[] nums) {
         long sum = 0;
+        int n = nums.length;
         for (int i = 0; i < n; i++) {
-            int left = i - psee[i];
-            int right = nse[i] - i;
-            sum += 1L * left * right * arr[i];
+            int max = nums[i];
+            int min = nums[i];
+            for (int j = i; j < n; j++) {
+                max = Math.max(max, nums[j]);
+                min = Math.min(min, nums[j]);
+                sum += (max - min);
+            }
         }
         return sum;
-    }
-
-    private long sumSubarrayMaxs(int[] arr) {
-        int n = arr.length;
-        int[] nge = findNGE(arr);
-        int[] pgee = findPGEE(arr);
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            int left = i - pgee[i];
-            int right = nge[i] - i;
-            sum += 1L * left * right * arr[i];
-        }
-        return sum;
-    }
-    public long subArrayRanges(int[] arr) {
-        return sumSubarrayMaxs(arr) - sumSubarrayMins(arr);
     }
 }
