6ab01aaf57cf53689b361351d4ea4fd7ae760d1d
Pranav Chavhan <pranavchavhan2470@gmail.com>
2026-09-11 09:54:35 +0530

    Time: 960 ms (22.48%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select p.product_id,
 coalesce(round(sum(p.price*u.units)/sum(u.units),2),0) as average_price
 from Prices p
